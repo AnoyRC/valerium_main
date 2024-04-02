d5644ace8b73b3186d1cb1d25a44e4968094ee2e
Gautam Raj <ashragautam25@gmail.com>
2024-04-02 19:25:54 +0530

    Add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,50 @@ module.exports = withMT({
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "highlight-pink": "#EA13F2",
+        "highlight-red": "#E6213A",
+
+        "text-off-white": "F2F2F3",
+        "text-gray": "#4D4A4F",
+        "text-dark": "#121212",
+        "text-pink": "#FCADFF",
+        "text-light-grey": "#9E9E9E",
+
+        "bg-off-black": "0F0F0F",
+        "bg-off-white": "#F9F9F9",
+
+        "shadown-purple": "#8264BE",
+        "shadow-red": "#DC6062",
+      },
+
+      backgroundImage: {
+        "gradient-primary": "linear-gradient(90deg, #EA13F2 0%, #E6213A 100%)",
+        "gradient-primary-light":
+          "linear-gradient(90deg, #EA13F2 0%, #E6213A 100%)",
+        "gradient-primary-radial":
+          "radial-gradient(1067.57% 100.06% at 0% 49.75%, #EA13F2 0%, #E6213A 60.4%, #EA13F2 100%)",
+        "gradient-primary-light-radial":
+          "radial-gradient(1067.57% 100.06% at 0% 49.75%, #C41CCB 0%, #C4182D 60.4%, #C41CCB 100%)",
+
+        "gradient-dark-linear":
+          "linear-gradient(180deg, #1E1E1E 0%, #141414 100.4%)",
+        "gradient-light-radial":
+          "linear-gradient(180deg, #E9E9E9 0%, #D8D8D8 100.4%)",
+      },
+    },
   },
 
   plugins: [
     plugin(function ({ addUtilities }) {
-      const newUtilities = {};
+      const newUtilities = {
+        ".gradient-text": {
+          "background-clip": "text",
+          "-webkit-background-clip": "text",
+          "-webkit-text-fill-color": "transparent",
+        },
+      };
 
       addUtilities(newUtilities);
     }),
