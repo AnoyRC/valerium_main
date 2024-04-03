73c23698c946ac7ee77a75d3a214c310c9ae1735
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-04-03 23:44:05 +0530

    Minor Bug Fixes

--- hooks/useLogin.js
+++ hooks/useLogin.js
@@ -6,19 +6,23 @@ import { ethers } from "ethers";
 
 export default function useLogin() {
   const getValerium = async (domain) => {
-    const provider = new ethers.providers.JsonRpcProvider(baseChain.rpcUrl);
+    try {
+      const provider = new ethers.providers.JsonRpcProvider(baseChain.rpcUrl);
 
-    const factory = new ethers.Contract(
-      baseChain.addresses.ValeriumProxyFactory,
-      ValeriumProxyFactoryABI,
-      provider
-    );
+      const factory = new ethers.Contract(
+        baseChain.addresses.ValeriumProxyFactory,
+        ValeriumProxyFactoryABI,
+        provider
+      );
 
-    const valeriumAddress = await factory.getValeriumProxy(
-      domain + "@valerium"
-    );
+      const valeriumAddress = await factory.getValeriumProxy(
+        domain + "@valerium"
+      );
 
-    return valeriumAddress;
+      return valeriumAddress;
+    } catch (error) {
+      return ethers.constants.AddressZero;
+    }
   };
 
   return { getValerium };
