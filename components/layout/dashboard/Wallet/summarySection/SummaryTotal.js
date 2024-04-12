342b43e2f17014251fa3570a38b878f8ae7c74ad
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-04-13 03:53:33 +0530

    Minor Change

--- components/layout/dashboard/Wallet/summarySection/SummaryTotal.js
+++ components/layout/dashboard/Wallet/summarySection/SummaryTotal.js
@@ -8,83 +8,85 @@ import { formatAmount } from "@/utils/formatAmount";
 const SummaryTotal = ({ token, usdToggle, isLoading, gas, amount }) => {
   const txProof = useSelector((state) => state.proof.txProof);
   const tokenConversionData = useSelector(
-    (state) => state.user.tokenConversionData,
+    (state) => state.user.tokenConversionData
   );
 
   const currentTokenConversion = token[0]
     ? tokenConversionData
       ? 1 /
           tokenConversionData.find(
-            (selectedToken) => selectedToken.address === token[0].address,
+            (selectedToken) => selectedToken.address === token[0].address
           ).usdValue || 1
       : 0
     : 0;
 
   return (
     <>
-      <div className="flex w-full justify-between">
-        <p className="text-base font-normal text-text-gray">
-          Total Amount to Pay:
-        </p>
+      <div>
+        <div className="flex w-full justify-between">
+          <p className="text-base font-normal text-text-gray">
+            Total Amount to Pay:
+          </p>
 
-        <p className="text-2.5xl font-bold text-black">
-          {isLoading ? (
-            <Loader2 className="h-5 w-5 animate-spin" />
-          ) : (
-            <>
-              {txProof && token[0].address === token[1].address
-                ? usdToggle
-                  ? (
-                      amount / Number(currentTokenConversion) +
-                      gas / 10 ** token[1].decimals
-                    ).toFixed(
-                      parseInt(
-                        (gas / 10 ** token[1].decimals)
-                          .toExponential()
-                          .split("e")[1],
-                      ) * -1,
-                    )
-                  : (Number(amount) + gas / 10 ** token[1].decimals).toFixed(
-                      parseInt(
-                        (gas / 10 ** token[1].decimals)
-                          .toExponential()
-                          .split("e")[1],
-                      ) * -1,
-                    )
-                : usdToggle
+          <p className="text-2.5xl font-bold text-black">
+            {isLoading ? (
+              <Loader2 className="h-5 w-5 animate-spin" />
+            ) : (
+              <>
+                {txProof && token[0].address === token[1].address
+                  ? usdToggle
+                    ? (
+                        amount / Number(currentTokenConversion) +
+                        gas / 10 ** token[1].decimals
+                      ).toFixed(
+                        parseInt(
+                          (gas / 10 ** token[1].decimals)
+                            .toExponential()
+                            .split("e")[1]
+                        ) * -1
+                      )
+                    : (Number(amount) + gas / 10 ** token[1].decimals).toFixed(
+                        parseInt(
+                          (gas / 10 ** token[1].decimals)
+                            .toExponential()
+                            .split("e")[1]
+                        ) * -1
+                      )
+                  : usdToggle
                   ? formatAmount(amount / Number(currentTokenConversion))
                   : amount || "0.00"}
-              <span> {token[0]?.symbol}</span>
-            </>
-          )}
-        </p>
-      </div>
-
-      {txProof && token[0].address !== token[1].address && !isLoading && (
-        <div className="flex w-full flex-row-reverse justify-between">
-          <p className="text-text-gray">
-            {!isLoading && usdToggle
-              ? "+ " +
-                (gas / 10 ** token[1].decimals).toFixed(
-                  parseInt(
-                    (gas / 10 ** token[1].decimals)
-                      .toExponential()
-                      .split("e")[1],
-                  ) * -1,
-                )
-              : "+ " +
-                (gas / 10 ** token[1].decimals).toFixed(
-                  parseInt(
-                    (gas / 10 ** token[1].decimals)
-                      .toExponential()
-                      .split("e")[1],
-                  ) * -1,
-                )}
-
-            <span> {token[1]?.symbol}</span>
+                <span> {token[0]?.symbol}</span>
+              </>
+            )}
           </p>
         </div>
-      )}
+
+        {txProof && token[0].address !== token[1].address && !isLoading && (
+          <div className="flex w-full flex-row-reverse justify-between">
+            <p className="text-text-gray">
+              {!isLoading && usdToggle
+                ? "+ " +
+                  (gas / 10 ** token[1].decimals).toFixed(
+                    parseInt(
+                      (gas / 10 ** token[1].decimals)
+                        .toExponential()
+                        .split("e")[1]
+                    ) * -1
+                  )
+                : "+ " +
+                  (gas / 10 ** token[1].decimals).toFixed(
+                    parseInt(
+                      (gas / 10 ** token[1].decimals)
+                        .toExponential()
+                        .split("e")[1]
+                    ) * -1
+                  )}
+
+              <span> {token[1]?.symbol}</span>
+            </p>
+          </div>
+        )}
+      </div>
     </>
   );
 };
